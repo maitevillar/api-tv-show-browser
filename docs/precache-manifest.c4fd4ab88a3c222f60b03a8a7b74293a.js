2ab77119455c9267935e363e72d0e91c03def244
maitevillar <sayhello.maite@gmail.com>
2020-06-03 12:59:25 +0200

    docs

---
+++ docs/precache-manifest.c4fd4ab88a3c222f60b03a8a7b74293a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b47530c3424526cc124cc80b37348c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79daf228896db20c7050",
+    "url": "./static/css/main.a6d6073f.chunk.css"
+  },
+  {
+    "revision": "c0b659f2cc00fa6bc0e3",
+    "url": "./static/js/2.1c8f6e11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1c8f6e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79daf228896db20c7050",
+    "url": "./static/js/main.1f50fae8.chunk.js"
+  },
+  {
+    "revision": "21f20cfa1e22351fb30f",
+    "url": "./static/js/runtime-main.f7363fe5.js"
+  }
+]);
